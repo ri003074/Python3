5791eb3aaedd6dd9f33121daa27d94e827339f1d
ri003074 <ri003074@gmail.com>
2021-01-29 21:35:35 +0900

    add skip

--- src/unittest/test_calculation.py
+++ src/unittest/test_calculation.py
@@ -1,11 +1,26 @@
 import unittest
 import calculation
 
+release_name = "lesson"
+
 
 class CalTest(unittest.TestCase):
+    def setUp(self):
+        print("set up")
+        self.cal = calculation.Cal()
+
+    def tearDown(self):
+        print("clean up")
+        del self.cal
+
+    # @unittest.skip("skip!")
+    @unittest.skipIf(release_name == "lesson", "skip!!")
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        self.assertEqual(cal.add_num_and_double(1, 1), 4)
+        self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
+
+    def test_add_num_and_double_raise(self):
+        with self.assertRaises(ValueError):
+            self.cal.add_num_and_double("1", "1")
 
 
 if __name__ == "__main__":
