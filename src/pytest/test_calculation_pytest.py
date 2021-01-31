ac2cc9f76c41dd77daad5a0796e22d01788adc3c
ri003074 <ri003074@gmail.com>
2021-01-31 08:21:12 +0000

    add setup

--- src/pytest/test_calculation_pytest.py
+++ src/pytest/test_calculation_pytest.py
@@ -8,14 +8,30 @@ import pytest
 
 
 class TestCal(object):
+    @classmethod
+    def setup_class(cls):
+        print("start")
+        cls.cal = calculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print("end")
+        del cls.cal
+
+    def setup_method(self, method):
+        print(f"method={method.__name__}")
+
+    def teardown_method(self, method):
+        print(f"method={method.__name__}")
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 4
+        # cal = calculation.Cal()
+        assert self.cal.add_num_and_double(1, 1) == 4
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double("1", "1")
+            # cal = calculation.Cal()
+            self.cal.add_num_and_double("1", "1")
             # cal.add_num_and_double(1, 1)
 
 
