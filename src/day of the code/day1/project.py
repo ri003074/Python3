f91d4e0f721133a24a1eb1dbca103657eb0d9010
ri003074 <ri003074@gmail.com>
2021-02-01 19:59:14 +0900

    add code

---
+++ src/day of the code/day1/project.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+print("What's name of the city you grew up in?")
+city = input()
+print("What's name of your pet?")
+pet = input()
+
+print(f"Your band name could be {city} {pet}")
---
+++ src/kwargs.py
@@ -0,0 +1,6 @@
+def greet_me(**kwargs):
+    for key, value in kwargs.items():
+        print("{0} = {1}".format(key, value))
+
+
+greet_me(name="kenta")
