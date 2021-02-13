27513e17d472b25361befb2b11ef670781ac75fb
ri003074 <ri003074@gmail.com>
2021-02-13 20:03:43 +0900

    update

--- src/excel/csv_to_excel_pandas.py
+++ src/excel/csv_to_excel_pandas.py
@@ -2,12 +2,13 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.chart import LineChart, Reference
 
-df = pd.read_csv("csv_sample.csv")
+input_file = "csv_sample.csv"
+output_file = "output.xlsx"
+df = pd.read_csv(input_file)
 df = df.loc[:, ~df.columns.str.match("Unnamed")]
-df.to_excel("csv_sample.xlsx", index=False)
+df.to_excel(output_file, index=False)
 
-
-wb = load_workbook("csv_sample.xlsx")
+wb = load_workbook(output_file)
 ws = wb.active
 
 values = Reference(ws, min_col=2, min_row=1, max_col=3, max_row=4)
@@ -20,4 +21,4 @@ chart.add_data(values, titles_from_data=True)
 chart.set_categories(categories)
 
 ws.add_chart(chart, "B4")
-wb.save("output.xlsx")
+wb.save(output_file)
