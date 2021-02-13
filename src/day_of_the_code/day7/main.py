537f342bb2814760d72fc5aa8328af5529073ffd
ri003074 <ri003074@gmail.com>
2021-02-14 08:22:59 +0900

    adD

--- src/day_of_the_code/day7/main.py
+++ src/day_of_the_code/day7/main.py
@@ -1,6 +1,9 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_art import stages, logo
+from hangman_words import word_list
+
+lives = 6
 
 
 # chosen_word = word_list[random.randint(0, len(word_list) - 1)]
@@ -11,6 +14,7 @@ display = []
 for _ in range(len(chosen_word)):
     display.append("_")
 
+print(logo)
 print(chosen_word)
 print(display)
 
@@ -19,13 +23,23 @@ end_of_game = False
 while not end_of_game:
     guess = input("Guess: ").lower()
 
+    if guess in display:
+        print(f"You already entered{guess}")
+
     for index, letter in enumerate(chosen_word):
         if letter == guess:
             display[index] = letter
 
-    print(display)
+    if guess not in chosen_word:
+        lives -= 1
 
-    if "_" not in display:
+    print(display)
+    print(stages[6 - lives])
+
+    if "_" not in display or lives == 0:
         end_of_game = True
 else:
-    print("You Win!!")
+    if lives > 0:
+        print("You Win!!")
+    else:
+        print("You lose")
