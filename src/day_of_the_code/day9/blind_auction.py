13f8ba0cb1f32eab37d96e4ec7ac16721266362e
ri003074 <ri003074@gmail.com>
2021-02-15 15:25:36 +0900

    update

--- src/day_of_the_code/day9/blind_auction.py
+++ src/day_of_the_code/day9/blind_auction.py
@@ -3,6 +3,25 @@ from art import logo
 
 print(logo)
 
-result = {"john": 100, "jun": 200, "jen": 300}
+result = {}
 
-winner = ""
+should_input = True
+while should_input:
+    username = input("name? : ")
+    bid = int(input("$ : "))
+    result[username] = bid
+
+    should_continue = input("next? yes or no : ")
+    if should_continue == "yes":
+        clear()
+    else:
+        should_input = False
+
+
+winner = username
+
+for name in result:
+    if result[name] > result[winner]:
+        winner = name
+
+print(winner)
