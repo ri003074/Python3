b57e74feba9e915d0db3915c650151c44479cf5d
ri003074 <ri003074@gmail.com>
2021-02-28 07:45:37 +0900

    clean up

--- src/pptx/picture_paste_class.py
+++ src/pptx/picture_paste_class.py
@@ -1,5 +1,5 @@
 from pptx import Presentation
-from pptx.util import Inches
+from pptx import util
 from PIL import Image
 import math
 
@@ -14,25 +14,32 @@ file_list = [
 ]
 
 file_path = "./pictures/"
+output_filename = "cc.pptx"
 
 
 class PowerPoint:
-    def __init__(self, files, file_path, pic_per_page, image_height, pic_top_offset=0):
-        self.file_path = file_path
+    def __init__(
+        self,
+        files,
+        file_path,
+        output_filename,
+        pic_per_page,
+        image_height,
+        pic_top_offset=0,
+    ):
         self.files = files
+        self.file_path = file_path
         self.slide_template = "sample_slide.pptx"
-        self.output_filename = "class_output.pptx"
+        self.output_filename = output_filename
         self.image_display_height = image_height
+        self.pic_per_page = pic_per_page
+        self.pic_top_offset = pic_top_offset
         self.image_display_width = 0
         self.aspect_ratio = 0
-        self.num_of_slides = 0
-        self.pic_per_page = pic_per_page
         self.presentaition = Presentation(self.slide_template)
         self.slide_width = self.presentaition.slide_width
         self.slide_height = self.presentaition.slide_height
         self.slide_layout = self.presentaition.slide_layouts[5]
-        self.pic_top_offset = pic_top_offset
-        # self.pic_top_offset = -1000000
 
     def calc_aspect_ratio(self, file_name):
         """ get image size and calculate aspect ratio"""
@@ -53,11 +60,11 @@ class PowerPoint:
             height=self.image_display_height,
         )
 
-    def add_title(self, slide, file_name):
+    def add_title(self, slide, text):
         """ add title to slide """
         title = slide.placeholders[0]
-        title.text = file_name
-        title.text = file_name.replace(".png", "").replace(".jpg", "")
+        title.text = text.replace(".png", "").replace(".jpg", "")
+        title.text_frame.paragraphs[0].font.size = util.Pt(20)
 
     def make_pptx(self):
         """ make powerpoint file """
@@ -74,8 +81,8 @@ class PowerPoint:
             print("Error")
 
     def make_pptx_one_pic_per_slide(self):
-        self.num_of_slides = len(self.files)
-        for i in range(0, self.num_of_slides, self.pic_per_page):
+        num_of_slides = len(self.files)
+        for i in range(0, num_of_slides, self.pic_per_page):
             slide = self.presentaition.slides.add_slide(self.slide_layout)
 
             file_name = self.files[i]
@@ -93,8 +100,8 @@ class PowerPoint:
         self.presentaition.save(self.output_filename)
 
     def make_pptx_two_or_more_pic_per_slide(self):
-        self.num_of_slides = int(math.ceil(len(self.files) / self.pic_per_page))
-        for i in range(0, self.num_of_slides * 2 + 3, self.pic_per_page):
+        num_of_slides = int(math.ceil(len(self.files) / self.pic_per_page))
+        for i in range(0, num_of_slides * 2 + 3, self.pic_per_page):
             slide = self.presentaition.slides.add_slide(self.slide_layout)
 
             for j in range(0, self.pic_per_page):
@@ -232,7 +239,7 @@ class PowerPoint:
                 self.add_picture(slide, file_name, left, top)
 
                 # add text
-                width = height = Inches(1)
+                width = height = util.Inches(1)
                 txBox = slide.shapes.add_textbox(left, top - 400000, width, height)
                 tf = txBox.text_frame
                 tf.text = file_name.replace(".png", "").replace(".jpg", "")
@@ -243,9 +250,10 @@ class PowerPoint:
 pptx = PowerPoint(
     files=file_list,
     file_path=file_path,
-    pic_per_page=2,
-    image_height=Inches(3.0),
-    # pic_top_offset=-1000000,
+    output_filename=file_path + output_filename,
+    pic_per_page=3,
+    image_height=util.Inches(2.0),
     pic_top_offset=0,
+    # pic_top_offset=-1000000,  # for 4 or 6 images for 1 slide
 )
 pptx.make_pptx()
