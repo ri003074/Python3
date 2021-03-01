c8b5b9bbfad37989c9995b1e4a110c135397f0a9
ri003074 <ri003074@gmail.com>
2021-03-01 21:33:20 +0900

    add

--- src/excel/csv_to_excel_pandas.py
+++ src/excel/csv_to_excel_pandas.py
@@ -8,36 +8,43 @@ df = pd.read_csv(input_file)
 df = df.loc[:, ~df.columns.str.match("Unnamed")]
 df.to_excel(output_file, index=False)
 
-df_dict = {}
+test_infos = []
 for name, group in df.groupby("test"):
-    df_dict[name] = group
     sheet = "test" + str(name)
     with pd.ExcelWriter("output.xlsx", engine="openpyxl", mode="a") as writer:
         group.to_excel(writer, sheet_name=sheet, index=False)
+        print(len(group.columns))
+        print(len(group.index))
+        test_infos.append(
+            {
+                "sheet_name": sheet,
+                "max_col": len(group.columns),
+                "max_row": len(group.index) + 1,
+            }
+        )
 
-# max_col = len(df.columns)
-# max_row = len(df.index) + 1
 
+wb = load_workbook(output_file)
 
-# for i in range(len(df_dict)):
-#    sheet = "sample" + str(i)
-#    with pd.ExcelWriter("output.xlsx", engine="openpyxl", mode="a") as writer:
-#        df_dict[i + 1].to_excel(writer, sheet_name=sheet, index=False)
-#        print(i)
+for test_info in test_infos:
+    sheet_name = test_info["sheet_name"]
+    ws = wb[sheet_name]
+    values = Reference(
+        ws,
+        min_col=2,
+        min_row=1,
+        max_col=test_info["max_col"],
+        max_row=test_info["max_row"],
+    )
+    categories = Reference(
+        ws, min_col=2, min_row=2, max_col=2, max_row=test_info["max_row"]
+    )
+    chart = LineChart()
+    chart.legend = None
+    chart.title = sheet_name
 
+    chart.add_data(values, titles_from_data=True)
+    chart.set_categories(categories)
 
-# wb = load_workbook(output_file)
-# ws = wb.active
-#
-# values = Reference(ws, min_col=2, min_row=1, max_col=max_col, max_row=max_row - 2)
-# categories = Reference(ws, min_col=2, min_row=2, max_col=2, max_row=max_row - 2)
-# chart = LineChart()
-# chart.legend = None
-# chart.title = "Fruits"
-#
-# chart.add_data(values, titles_from_data=True)
-# chart.set_categories(categories)
-#
-# ws.add_chart(chart, "B4")
-# wb.save(output_file)
-#
+    ws.add_chart(chart, "B4")
+wb.save(output_file)
