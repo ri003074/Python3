1892404d29afebb1466502ebfe5ca6f0b83500f1
ri003074 <ri003074@gmail.com>
2021-03-06 18:15:27 +0900

    update

--- src/utils/graph/make_image.py
+++ src/utils/graph/make_image.py
@@ -5,21 +5,43 @@ data_path = "../data/"
 
 class Image:
     def __init__(self):
-        pass
+        self.markers = [".", "o", "^", "X", "D"]
 
     def make_graph(
-        self, input_file, output_file, yaxis_min, yaxis_max, ytics, graph_title=""
+        self,
+        input_file,
+        output_file,
+        yaxis_min,
+        yaxis_max,
+        ytics,
+        graph_title="",
+        xlabel="",
+        ylabel="",
     ):
         df = pandas.read_csv(input_file, index_col=[0])
+        print(len(df.index))
         ax = df.plot(
-            figsize=(10, 10),
+            kind="line",
+            figsize=(16, 9),
             title=graph_title,
             legend=True,
             ylim=[yaxis_min, yaxis_max],
             yticks=ytics,
+            xticks=range(0, len(df.index)),
+            marker="o",
+            fontsize=20,
         )
-        ax.set_ylabel("mV")
-        # ax.set_xlabel("mV")
+        ax.grid(axis="y")
+
+        # ax.annotate("anontetion", xy=(2, 6))
+
+        for i, line in enumerate(ax.get_lines()):
+            line.set_marker(self.markers[i])
+            ax.legend(ax.get_lines(), df.columns, loc="best")
+
+        ax.set_ylabel(ylabel, fontsize=20)
+        ax.legend(fontsize=20, loc="best")
+        ax.set_xlabel(xlabel)
         fig = ax.get_figure()
         fig.savefig(output_file)
 
@@ -31,13 +53,15 @@ if __name__ == "__main__":
         input_file=data_path + "csv_type1_1.csv",
         output_file="csv_type1_1.png",
         yaxis_min=0,
-        yaxis_max=10,
-        ytics=range(0, 12, 2),
+        yaxis_max=8,
+        ylabel="mV",
+        ytics=range(0, 10, 2),
     )
     image.make_graph(
         input_file=data_path + "csv_type1_2.csv",
         output_file="csv_type1_2.png",
         yaxis_min=0,
-        yaxis_max=20,
-        ytics=range(0, 22, 2),
+        yaxis_max=10,
+        ylabel="mV",
+        ytics=range(0, 12, 2),
     )
