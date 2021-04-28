da0180eb8bcd04615fbf39444f8a1b557b62dbcc
kenta <ri003074@gmail.com>
2021-04-29 06:19:10 +0900

    update

--- src/pptx/pptx_win32com.py
+++ src/pptx/pptx_win32com.py
@@ -6,71 +6,105 @@ pptx = win32com.client.Dispatch("PowerPoint.Application")  # active presentation
 # pptx = win32com.client.GetObject(Class="PowerPoint.Application") # activate pptx
 
 active_presentation = pptx.ActivePresentation
-pptx_file_name = active_presentation.Name
-print(pptx_file_name)
 
-pptx_width = active_presentation.PageSetup.SlideWidth
-pptx_height = active_presentation.PageSetup.SlideHeight
-image_width = (pptx_width / 2) * 0.8
-image_left_position_offset = pptx_width / 2 * 0.1
-image_top_position = pptx_height * 0.4
-textbox_top_position = image_top_position - 50
-textbox_width = 200
-textbox_height = 100
-
-slide_count = active_presentation.Slides.Count
+PPTX_WIDTH = active_presentation.PageSetup.SlideWidth
+PPTX_HEIGHT = active_presentation.PageSetup.SlideHeight
+IMAGE_WIDTH = (PPTX_WIDTH / 2) * 0.8
+IMGAGE_LEFT_POSITION = PPTX_WIDTH / 2 * 0.1
+IMAGE_TOP_POSITION = PPTX_HEIGHT * 0.4
+TEXTBOX_TOP_POSITION = IMAGE_TOP_POSITION - 50
+TEXTBOX_WIDTH = 200
+TEXTBOX_HEIGHT = 100
+CURRENT_SLIDE_COUNT = active_presentation.Slides.Count
 images = glob.glob("imgs/*")
+images2 = glob.glob("imgs2/*")
+slide_titles = ["abc", "def"]
 
-image_counter = 0
-for i in range(slide_count + 1, slide_count + 1 + int(len(images) / 2)):
-    active_presentation.Slides.Add(i, 11)
 
-    # title
-    active_presentation.Slides(i).Shapes.Placeholders.Item(
-        1
-    ).TextFrame.TextRange.Text = "abc"
+def insert_images_to_pptx(slide_titles, *argv):
 
-    # first image and file name
-    inserted_txtbox = active_presentation.Slides(i).Shapes.AddTextbox(
-        1,
-        pptx_width / 4 - textbox_width / 2,
-        textbox_top_position,
-        textbox_width,
-        textbox_width,
-    )
-    image_name = images[image_counter]
-    image_name = image_name.replace("imgs\\", "").replace(".PNG", "")
-    inserted_txtbox.TextFrame.TextRange.Text = image_name
-    inserted_txtbox.TextFrame.TextRange.ParagraphFormat.Alignment = 2  # 中央ぞろえ
+    images_per_slide = len(argv)
 
-    inserted_imgs = active_presentation.Slides(i).Shapes.AddPicture(
-        FileName=os.getcwd() + "/" + images[image_counter],
-        LinkToFile=-1,
-        SaveWithDocument=-1,
-        Left=image_left_position_offset,
-        Top=image_top_position,
-    )
-    inserted_imgs.Width = image_width
-    image_counter += 1
+    image_slide_start_number = CURRENT_SLIDE_COUNT + 1
+    image_counter = 0
+    if images_per_slide == 1:
+        for i in range(
+            image_slide_start_number, image_slide_start_number + len(argv[0]),
+        ):
+            active_presentation.Slides.Add(i, 11)
+            # title
+            image_name = argv[0][image_counter]
+            image_name = image_name.replace("imgs\\", "").replace(".PNG", "")
+            active_presentation.Slides(i).Shapes.Placeholders.Item(
+                1
+            ).TextFrame.TextRange.Text = slide_titles[i - CURRENT_SLIDE_COUNT - 1]
 
-    # second image and file name
-    inserted_txtbox = active_presentation.Slides(i).Shapes.AddTextbox(
-        1,
-        pptx_width * 3 / 4 + -textbox_width / 2,
-        textbox_top_position,
-        textbox_width,
-        textbox_height,
-    )
-    image_name = images[image_counter]
-    image_name = image_name.replace("imgs\\", "").replace(".PNG", "")
-    inserted_txtbox.TextFrame.TextRange.Text = image_name
-    inserted_txtbox.TextFrame.TextRange.ParagraphFormat.Alignment = 2  # 中央ぞろえ
-    inserted_imgs = active_presentation.Slides(i).Shapes.AddPicture(
-        FileName=os.getcwd() + "/" + images[image_counter],
-        LinkToFile=-1,
-        SaveWithDocument=-1,
-        Left=pptx_width / 2 + image_left_position_offset,
-        Top=image_top_position,
-    )
-    inserted_imgs.Width = image_width
-    image_counter += 1
+            # first image and file name
+            inserted_imgs = active_presentation.Slides(i).Shapes.AddPicture(
+                FileName=os.getcwd() + "/" + images[image_counter],
+                LinkToFile=-1,
+                SaveWithDocument=-1,
+                Left=IMGAGE_LEFT_POSITION,
+                Top=IMAGE_TOP_POSITION,
+            )
+            inserted_imgs.Width = IMAGE_WIDTH
+            image_counter += 1
+
+    elif images_per_slide == 2:
+        for i in range(
+            image_slide_start_number, image_slide_start_number + len(argv[0]),
+        ):
+            active_presentation.Slides.Add(i, 11)
+            # title
+            image_name = argv[0][image_counter]
+            # first image and file name
+            inserted_imgs = active_presentation.Slides(i).Shapes.AddPicture(
+                FileName=os.getcwd() + "/" + image_name,
+                LinkToFile=-1,
+                SaveWithDocument=-1,
+                Left=IMGAGE_LEFT_POSITION,
+                Top=IMAGE_TOP_POSITION,
+            )
+            inserted_imgs.Width = IMAGE_WIDTH
+
+            image_name = image_name.replace("imgs\\", "").replace(".PNG", "")
+            active_presentation.Slides(i).Shapes.Placeholders.Item(
+                1
+            ).TextFrame.TextRange.Text = slide_titles[i - CURRENT_SLIDE_COUNT - 1]
+            inserted_txtbox = active_presentation.Slides(i).Shapes.AddTextbox(
+                1,
+                PPTX_WIDTH * 1 / 4 + -TEXTBOX_WIDTH / 2,
+                TEXTBOX_TOP_POSITION,
+                TEXTBOX_WIDTH,
+                TEXTBOX_HEIGHT,
+            )
+            inserted_txtbox.TextFrame.TextRange.Text = image_name
+            inserted_txtbox.TextFrame.TextRange.ParagraphFormat.Alignment = 2  # 中央ぞろえ
+
+            # title
+            image_name = argv[1][image_counter]
+            # second image and file name
+            inserted_imgs = active_presentation.Slides(i).Shapes.AddPicture(
+                FileName=os.getcwd() + "/" + image_name,
+                LinkToFile=-1,
+                SaveWithDocument=-1,
+                Left=IMGAGE_LEFT_POSITION + PPTX_WIDTH / 2,
+                Top=IMAGE_TOP_POSITION,
+            )
+            inserted_imgs.Width = IMAGE_WIDTH
+            image_name = image_name.replace("imgs2\\", "").replace(".PNG", "")
+            inserted_txtbox = active_presentation.Slides(i).Shapes.AddTextbox(
+                1,
+                PPTX_WIDTH * 3 / 4 + -TEXTBOX_WIDTH / 2,
+                TEXTBOX_TOP_POSITION,
+                TEXTBOX_WIDTH,
+                TEXTBOX_HEIGHT,
+            )
+            inserted_txtbox.TextFrame.TextRange.Text = image_name
+            inserted_txtbox.TextFrame.TextRange.ParagraphFormat.Alignment = 2  # 中央ぞろえ
+
+            image_counter += 1
+
+
+insert_images_to_pptx(slide_titles, images, images2)
+# insert_images_to_pptx(slide_titles, images)
