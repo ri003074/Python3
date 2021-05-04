3012788f6b150c6b168de7cda8c8214c64376d8d
kenta <ri003074@gmail.com>
2021-05-05 07:26:38 +0900

    update

--- src/pandas/samples_dataframe.py
+++ src/pandas/samples_dataframe.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 
 # data = [
 #     ["p1", 3, 1, 2],
@@ -16,9 +16,34 @@ import pandas as pd
 # print(df)
 
 
-csv_type2_1 = [[1, "p1", 1], [1, "p2", 4], [2, "p3", 3], [2, "p4", 5]]
+# csv_type2_1 = [[1, "p1", 1], [1, "p2", 4], [2, "p3", 3], [2, "p4", 5]]
 
-df = pd.DataFrame(csv_type2_1, columns=["test", "pin", "data"])
+# df = pd.DataFrame(csv_type2_1, columns=["test", "pin", "data"])
 
-for index, group in df.groupby("test"):
-    print(group)
+# for index, group in df.groupby("test"):
+#     print(group)
+
+import pandas as pd
+
+data = [
+    ["p1", 1],
+    ["p2", 6],
+    ["p3", 3],
+    ["p4", 4],
+]
+
+
+df = pd.DataFrame(data, columns=["pin", "data"])
+df_merge = pd.concat([df, df])
+print(df_merge.reset_index(drop=True))
+
+# df = pd.DataFrame(data, columns=["pin", "data"])
+# df1 = df[::2].reset_index(drop=True)
+# df2 = df[1::2].reset_index(drop=True)
+
+
+# diff_pin = df1["pin"] + "-" + df2["pin"]
+# diff_data = df1["data"] - df2["data"]
+
+# df_merge = pd.concat([diff_pin, diff_data], axis=1)
+# print(df_merge)
