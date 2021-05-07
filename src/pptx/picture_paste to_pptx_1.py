d720319677d8ad3e54f6eb568ff2fa0f7b990da3
ri003074 <ri003074@gmail.com>
2021-05-08 07:22:13 +0900

    update

--- src/pptx/picture_paste to_pptx_1.py
+++ src/pptx/picture_paste to_pptx_1.py
@@ -4,7 +4,7 @@ from PIL import Image
 import glob
 
 
-file_names = glob.glob("./imgs/*.PNG")
+FILE_NAMES = glob.glob("./imgs/*.PNG")
 
 IMG_DISPLAY_HEIGHT_CM = 10
 IMG_DISPLAY_HEIGHT = Inches(IMG_DISPLAY_HEIGHT_CM / 2.54)
@@ -16,13 +16,13 @@ prs = Presentation()
 SLIDE_WIDTH = prs.slide_width
 SLIDE_HEIGHT = prs.slide_height
 
-for i in range(0, len(file_names)):
+for i in range(0, len(FILE_NAMES)):
     blank_slide_layout = prs.slide_layouts[5]
     slide = prs.slides.add_slide(blank_slide_layout)
     title_placeholder = slide.shapes.title
 
     # 貼り付ける画像ファイル名を取得
-    file_name = file_names[i]
+    file_name = FILE_NAMES[i]
     title_placeholder.text = file_name
 
     # 画像サイズを取得してアスペクト比を得る
