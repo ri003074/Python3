f458724470466e24122cd3436ff0e3c2c9b7e04d
ri003074 <ri003074@gmail.com>
2021-05-10 20:12:11 +0900

    update select dir

--- src/utils/gui/select_dir.py
+++ src/utils/gui/select_dir.py
@@ -6,8 +6,13 @@ class SelectDir(wx.Frame):
         super(SelectDir, self).__init__(parent, title=title)
 
         self.folder = [""] * folder_count
-        self.tc = [""] * folder_count
         self.folder_count = folder_count
+        self.tc = []
+        self.arr = []
+        self.btn = []
+        self.st = []
+        self.rbox = []
+        self.btn_exe = []
         self.label1 = ["center"]
         self.label2 = ["left", "right"]
         self.label3 = ["left", "center", "right"]
@@ -20,42 +25,68 @@ class SelectDir(wx.Frame):
     def InitUI(self):
 
         hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
 
+        fgs = wx.FlexGridSizer(self.folder_count + 1, 3, 10, 10)
         fgs = wx.FlexGridSizer(self.folder_count + 1, 3, 10, 10)
 
         blank1 = wx.StaticText(self, label="")
-        blank2 = wx.StaticText(self, label="")
+        # blank2 = wx.StaticText(self, label="")
 
-        btn = []
-        st = []
-        arr = []
+        for tc in self.tc:
+            tc.Destroy()
+            pass
+
+        for btn in self.btn:
+            btn.Destroy()
+
+        for st in self.st:
+            st.Destroy()
+
+        self.btn = []
+        self.st = []
+        self.arr = []
+        self.tc = []
         for i in range(self.folder_count):
-            self.tc[i] = wx.TextCtrl(self, size=(400, -1))
-            st.append(
+            self.tc.append(wx.TextCtrl(self, size=(400, -1)))
+            self.st.append(
                 wx.StaticText(self, label=eval(f"self.label{self.folder_count}[i]"))
             )
-            btn.append(wx.Button(self, label="browse"))
-            btn[i].Bind(wx.EVT_BUTTON, self.file_browse)
-            btn[i].index = i
-            arr.append(st[i])
-            arr.append((self.tc[i], 1, wx.EXPAND))
-            arr.append(btn[i])
+            self.btn.append(wx.Button(self, label="browse"))
+            self.btn[i].Bind(wx.EVT_BUTTON, self.file_browse)
+            self.btn[i].index = i
+            self.arr.append(self.st[i])
+            self.arr.append([self.tc[i], 1, wx.EXPAND])
+            self.arr.append(self.btn[i])
 
-        btn_exe = wx.Button(self, label="execute")
-        btn_exe.Bind(wx.EVT_BUTTON, self.close)
+        if self.btn_exe:
+            self.btn_exe.Destroy()
 
-        arr.append(blank1)
-        arr.append(blank2)
-        arr.append(btn_exe)
-        fgs.AddMany(arr)
+        self.btn_exe = wx.Button(self, label="execute")
+        self.btn_exe.Bind(wx.EVT_BUTTON, self.close)
+
+        if self.rbox:
+            self.rbox.Destroy()
+
+        self.rbox = wx.RadioBox(self, -1, choices=["1", "2", "3", "4"])
+        self.rbox.Bind(wx.EVT_RADIOBOX, self.check)
+        self.rbox.EnableItem(self.folder_count - 1, False)
+        self.rbox.SetSelection(self.folder_count - 1)
+
+        self.arr.append(self.rbox)
+        self.arr.append(blank1)
+        self.arr.append(self.btn_exe)
+        fgs.AddMany(self.arr)
         fgs.AddGrowableCol(1, 1)
         hbox.Add(fgs, proportion=2, flag=wx.ALL | wx.EXPAND, border=10)
         self.SetSizer(hbox)
         self.Fit()
+        self.SetSizer(hbox)
+        self.Fit()
 
     def file_browse(self, event):
-        btn = event.GetEventObject()
-        index = btn.index
+        self.btn = event.GetEventObject()
+        index = self.btn.index
         folder = wx.DirDialog(self, style=wx.DD_CHANGE_DIR)
         if folder.ShowModal() == wx.ID_OK:
             folder_path = folder.GetPath()
@@ -66,10 +97,15 @@ class SelectDir(wx.Frame):
     def close(self, event):
         self.Close()
 
+    def check(self, event):
+        num = self.rbox.GetSelection()
+        self.folder_count = num + 1
+        self.InitUI()
+
 
 if __name__ == "__main__":
     app = wx.App()
-    select_dir = SelectDir(None, title="select dir", folder_count=3)
+    select_dir = SelectDir(None, title="select dir", folder_count=4)
     app.MainLoop()
     print(select_dir.folder[0])
     print(select_dir.folder[1])
--- src/wxPython/sample_gridsizer.py
+++ src/wxPython/sample_gridsizer.py
@@ -5,27 +5,86 @@ class Example(wx.Frame):
     def __init__(self, parent, title):
         super(Example, self).__init__(parent, title=title)
 
+        self.subs = []
+        self.rbox = []
+        self.checked_box = 0
+        self.InitUI()
         self.InitUI()
         self.Centre()
 
     def InitUI(self):
         vbox = wx.BoxSizer(wx.VERTICAL)
+
+        for a in self.subs:
+            print(type(a))
+            a.Destroy()
+
+        self.subs = []
+        if self.rbox:
+            self.rbox.Destroy()
+        self.rbox = wx.RadioBox(self, -1, choices=["choice 1", "choice 2"])
+        self.rbox.Bind(wx.EVT_RADIOBOX, self.check)
+        self.rbox.EnableItem(self.checked_box, False)
+        self.rbox.SetSelection(0)
         # self.display = wx.TextCtrl(self, style=wx.TE_RIGHT)
         # vbox.Add(self.display, flag=wx.EXPAND)
-        gs = wx.GridSizer(2, 3, 10, 10)
-        gs.AddMany(
-            [
-                (wx.Button(self, label="bck0")),
-                (wx.Button(self, label="bck1")),
-                (wx.Button(self, label="bck2")),
-                (wx.Button(self, label="bck3")),
-                (wx.Button(self, label="bck4")),
-                (wx.Button(self, label="bck5")),
-            ]
-        )
+        self.subs.append(self.rbox)
+        self.subs.append(wx.Button(self, label="bck0"))
+        self.subs.append(wx.Button(self, label="bck1"))
+        self.subs.append(wx.Button(self, label="bck2"))
+        self.subs.append(wx.Button(self, label="bck3"))
+        self.subs.append(wx.Button(self, label="bck4"))
+        self.subs.append(wx.Button(self, label="bck5"))
+        gs = wx.GridSizer(2, 4, 10, 10)
+        gs.AddMany(self.subs)
         vbox.Add(gs, proportion=1, flag=wx.EXPAND | wx.ALL, border=20)
         self.SetSizer(vbox)
         self.Fit()
+
+    def InitUI2(self):
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        # self.display = wx.TextCtrl(self, style=wx.TE_RIGHT)
+        # vbox.Add(self.display, flag=wx.EXPAND)
+        for a in vbox:
+            print(a)
+        gs = wx.GridSizer(1, 4, 10, 10)
+
+        for a in self.subs:
+            print(type(a))
+            a.Destroy()
+
+        self.subs = []
+        if self.rbox:
+            self.rbox.Destroy()
+        self.rbox = wx.RadioBox(self, -1, choices=["choice 1", "choice 2"])
+        self.rbox.Bind(wx.EVT_RADIOBOX, self.check)
+        self.rbox.EnableItem(self.checked_box, False)
+        self.rbox.SetSelection(1)
+
+        self.subs.append(self.rbox)
+        self.subs.append(wx.Button(self, label="bck0"))
+        self.subs.append(wx.Button(self, label="bck1"))
+        self.subs.append(wx.Button(self, label="bck2"))
+
+        gs.AddMany(self.subs)
+        vbox.Add(gs, proportion=1, flag=wx.EXPAND | wx.ALL, border=20)
+        self.SetSizer(vbox)
+        self.Fit()
+
+    def check(self, evt):
+        num = self.rbox.GetSelection()
+        self.checked_box = num
+        # for i in range(self.chce.GetCount()):
+        #     self.chce.Delete(0)
+        if num == 0:
+            # for i in range(3):
+            #     self.chce.Append(self.cc1[i])  # ここでリストを変える。
+            self.InitUI()
+        else:
+            # for i in range(2):
+            #     self.chce.Append(self.cc2[i])  # ここでリストを変える。
+            self.InitUI2()
+        # self.chce.SetSelection(0)  # リストの最初の項目を表示する
 
 
 def main():
